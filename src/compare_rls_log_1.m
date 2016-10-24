9fde3e5a08582891ef005bb5511afcc2de4e34dc
Qihong Lu <lvqihong1992@gmail.com>
2016-10-23 23:02:29 -0500

    clean up: compare boundary - ols vs. log

--- src/compare_rls_log_1.m
+++ src/compare_rls_log_1.m
@@ -14,21 +14,21 @@ center2 = [-3,-1];
 X1 = variance1 * bsxfun(@plus, randn(M, N), center1);
 X2 = variance2 * bsxfun(@plus, randn(M, N), center2);
 X = [X1; X2];  
-X_aug = [ones(2*M, 1) [X1; X2]];    % add intercept term
+X_aug = [ones(2*M, 1) X];    % add intercept term
 y = [ones(M, 1); -ones(M, 1)];
 
-%% Solve least squares problem
+%% Solve least square & logistic optimization 
 beta.ls = inv(X_aug' * X_aug) * X_aug' * y; 
 fit = cvglmnet(X,y, 'binomial');
-
 beta.log = cvglmnetCoef(fit,'lambda_min');
 
-%% Plot data points and OLS boundary.ls
+%% Plot data points and boundary
+% compute the boundaries
 bound = 3; 
 x = -bound:.1:bound;
 boundary.ls = -beta.ls(1)/beta.ls(3) - (beta.ls(2)/beta.ls(3))*x;
 boundary.log = -beta.log(1)/beta.log(3) - (beta.log(2)/beta.log(3))*x;
-
+% plot 
 hold on;
 plot(X1(:, 1), X1(:, 2), 'kx', 'linewidth', 2); 
 plot(X2(:, 1), X2(:, 2), 'ko', 'linewidth', 2); 
